72e8f62c0c2d7e6d6da319fc3767d7176e81a06a
Ellen Lippe <ellen.lippe@yahoo.com>
2015-09-21 14:45:58 +0200

    message url in env variable

--- routes/store.js
+++ routes/store.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 
 router.get('/', function(req, res, next) {
-  res.render('store', { title: 'Store' });
+    console.log(process.env.message_url)
+  res.render('store', { title: 'Store', messageURL: process.env.message_url});
 });
+
+router.route('/message').post(function(req, res, next) {
+      console.log('Retrieving message' + req.body.message)
+      res.send('Message was '+req.body.message);
+});
+
 
 module.exports = router;
