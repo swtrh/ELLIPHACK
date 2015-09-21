4ed990fb2bc9ce664fca93ccc9cd2afa60e7b5bc
Ellen Lippe <ellen.lippe@yahoo.com>
2015-09-21 11:37:09 +0200

    port variable

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var port = (process.env.PORT || '3000');
 
 app.get('/', function(req, res) {
 	res.send('Hello World')
@@ -9,5 +10,5 @@ app.get('/help/', function(req, res) {
 	res.send('Help! I need somebody')
 })
 
-console.log('Starting on port 3000')
-app.listen(3000)
+console.log('Starting on port' + port)
+app.listen(port)
